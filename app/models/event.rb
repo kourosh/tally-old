546a562ebebba95876e0e1d795a2c1f31cf575f5
Kourosh Karimkhany <kouroshk@gmail.com>
2014-12-14 11:09:00 -0800

    Implement politician drop-down menu on new event view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
 	belongs_to :pol
 
+	def pol_fullname
+		pol.try(:lastname)
+	end
+
+	def pol_fullname=(lastname)
+		self.pol = Pol.find_by_name(lastname) if lastname.present?
+	end
+
 end
