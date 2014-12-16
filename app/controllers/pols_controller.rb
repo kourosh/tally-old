e82cc73ef5dc60b8d1634be6abd8577b174a83b8
Kourosh Karimkhany <kouroshk@gmail.com>
2014-12-16 11:53:39 -0800

    Change pol controller to render pol and events in json for index

--- app/controllers/pols_controller.rb
+++ app/controllers/pols_controller.rb
@@ -8,7 +8,7 @@ class PolsController < ApplicationController
     @pols = Pol.includes(:events).all
     respond_to do |format|
       format.html
-      format.json { render json: @pols, status: 200 }
+      format.json { render :json => { bio: @pol, events: @pol.events } }
     end
   end
 
