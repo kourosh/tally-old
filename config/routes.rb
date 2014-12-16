1937e1e5c4bdb10ed60c6e4fd0a6e962190f109b
Kourosh Karimkhany <kouroshk@gmail.com>
2014-12-16 15:27:36 -0800

    Delete api subdomain, add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
   
-  resources :events, except: [:new, :edit, :update, :destroy], constraints: { subdomain: 'api' }
+	namespace :api, path: '/' do
+  	resources :events, :search_suggestions, :users, :pols
+	end
+
+
+
+  resources :events, except: [:new, :edit, :update, :destroy]
 
   resources :events
 
-  resources :search_suggestions, except: [:new, :edit], constraints: { subdomain: 'api' }
+  resources :search_suggestions, except: [:new, :edit]
 
-  resources :users, except: [:new, :edit], constraints: { subdomain: 'api' }
+  resources :users, except: [:new, :edit]
 
-  resources :pols, except: [:new, :edit, :update, :destroy], constraints: { subdomain: 'api' }
+  resources :pols, except: [:new, :edit, :update, :destroy]
 
   resources :pols
   
-  post '/users/signup' => 'users#create', constraints: { subdomain: 'api' }
+  post '/users/signup' => 'users#create'
 
-  post '/users/login' => 'users#attempt_login', constraints: { subdomain: 'api' }
-
+  post '/users/login' => 'users#attempt_login'
+  
 end
