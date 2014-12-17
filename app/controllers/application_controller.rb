73c599d1972bdf41a54add2533e8748d4c2afbd3
Joshua Notowitz <joshnotowitz@gmail.com>
2014-12-17 12:25:13 -0800

    payment info modal opening when thing happens that we want it to oppen when that thing happens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception,
 	if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
-  # def confirgurebraintree
-  #   Braintree::Configuration.environment = ENV["braintree_environment"]
-  #   Braintree::Configuration.merchant_id = ENV["braintree_merchant_id"]
-  #   Braintree::Configuration.public_key = ENV["braintree_public_key"]
-  #   Braintree::Configuration.private_key = ENV["braintree_private_key"]
-  # end
+  def confirgurebraintree
+    Braintree::Configuration.environment = ENV["braintree_environment"]
+    Braintree::Configuration.merchant_id = ENV["braintree_merchant_id"]
+    Braintree::Configuration.public_key = ENV["braintree_public_key"]
+    Braintree::Configuration.private_key = ENV["braintree_private_key"]
+  end
 end
