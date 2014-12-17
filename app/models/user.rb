09b50b0e56dbb6308565d5bb422adc11ac87d4f5
Myla Baker <myla.baker@gmail.com>
2014-12-16 20:00:06 -0800

    fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
 	:lng_column_name => :longitude
 	has_secure_password
 
-	validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
