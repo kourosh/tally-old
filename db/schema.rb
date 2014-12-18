0afbe2fbed5a43e499bbe24219d76a61817de713
Joshua Notowitz <joshnotowitz@gmail.com>
2014-12-17 16:49:31 -0800

    voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217225508) do
+ActiveRecord::Schema.define(version: 20141217233820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20141217225508) do
     t.integer  "pol_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "balance_for"
+    t.float    "balance_against"
   end
 
   create_table "pols", force: true do |t|
