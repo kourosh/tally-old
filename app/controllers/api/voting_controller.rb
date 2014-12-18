f400b959d97b0f9950cb6cd96281660c25ae93e4
Joshua Notowitz <joshnotowitz@gmail.com>
2014-12-17 19:04:54 -0800

    everything is working now1111111

--- app/controllers/api/voting_controller.rb
+++ app/controllers/api/voting_controller.rb
@@ -1,10 +1,50 @@
 module API
 	class VotingController < ApplicationController
 		def support
-			
+			user = User.find(params[:uid])
+			event = Event.find(params[:eid])
+			if user.balance == nil 
+				user.balance = 100
+			end
+			if event.balance_for == nil
+				event.balance_for = 0
+			end
+			if user.balance > 0
+				user.balance += -5
+				event.balance_for += 5
+				user.save
+				event.save
+				respond_to do |format|
+					format.json { head :no_content }
+				end
+			else
+				respond_to do |format|
+					format.json { render :json => { value: "YOU RAN OUT OF MONEY BRO" } }
+				end
+			end
 		end
-
 		def oppose
+			user = User.find(params[:uid])
+			event = Event.find(params[:eid])
+			if user.balance == nil
+				user.balance = 100
+			end
+			if event.balance_against == nil
+				event.balance_against = 0
+			end
+			if user.balance > 0
+				user.balance += -5
+				event.balance_against += 5
+				user.save
+				event.save
+				respond_to do |format|
+					format.json { head :no_content }
+				end
+			else
+				respond_to do |format|
+					format.json { render :json => { value: "YOU RAN OUT OF MONEY BRO" } }
+				end
+			end
 		end
 	end
 end
